3fc1b7611550de67ddcf5aa7cdc1ee897161190b
Nasir Osman <nmosman@ucalgary.ca>
2018-03-06 15:53:59 -0700

    update main.py

--- pi_os/main/main.py
+++ pi_os/main/main.py
@@ -2,12 +2,12 @@ import pybluez
 import bluetooth
 import time 
 
-#TODO: Testing!!!!!
-
-
+#TODO: Testing!!!!
+BUFFER_SIZE = 256
 
 def setup_gpio_pins():
     # We can play around with what does what and add more here
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
@@ -17,26 +17,31 @@ audio_buffer = []
 def read_buffer():
     return -1
 
-if name == "__main__":
-    GPIO.setmode(GPIO.BCM)  
-  
+if name == "__main__":   
+    setup_gpio_pins()
+    while 1: 
+        # Write high to issue sending cmd 
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(0.001)
+        
+       # read 256 bytes from GPIO18
     
-    # Write high to issue sending cmd 
-    GPIO.output(18, GPIO.HIGH)
-    time.sleep(0.001)
-    
-    
-    
-      
-    
-    
-    
-    
-    
-    if GPIO.input(17):
-        print("Pin 11 is HIGH")
-    else:
-        print("Pin 11 is LOW")
-    GPIO.cleanup()
-    
-    
+       # perform the heart rate algo.
+        
+   
+       # send out heart rate via bluetooth
+       
+
+
+
+
+
+
+
+        if GPIO.input(17):
+            print("Pin 11 is HIGH")
+        else:
+            print("Pin 11 is LOW")
+        GPIO.cleanup()
+
+
