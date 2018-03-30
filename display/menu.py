f69e2626130394d072d06f83b28ba43ff3274768
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2018-03-30 14:21:17 -0600

    More animations stuff

--- display/menu.py
+++ display/menu.py
@@ -4,10 +4,16 @@
 
 import sys
 import math
+import time
+import numpy as np
 from time import sleep
 from demo_opts import get_device
 from luma.core.render import canvas
 from luma.core.sprite_system import framerate_regulator
+
+# For connecting to the physical device
+from luma.core.interface.serial import i2c, spi
+from luma.oled.device import ssd1306
 
 
 def radians(degrees):
@@ -21,51 +27,63 @@ def bounce_animation():
         origin = (origin + 3.5) % 360 # Essentially same as += 1 for sin
 
 
-class Menu(object):
-    """Main menu"""
-    def __init__(self, menu_options):
-        assert len(menu_options["options"]) <= 6 # Limit to 6 options
-        self.options = menu_options
-        self.ha = bounce_animation()
+def main():
+    device = get_device()
 
-    def run(self, device, updated_options=None):
-        if updated_options is not None:
-            assert len(updated_options["options"]) <= 6 # Limit to 6 options
-            self.options = updated_options
+    while True:
+        prev_time = time.time()
 
-        with canvas(device, dither=True) as draw:
-            # Menu options
-            for i, option in enumerate(self.options["options"]):
-                draw.text((50, 2+i*10), option, fill="white")
-                if i == self.options["cursor"]:
-                    draw.text((40, 2+i*10), ">", fill="white")
+        count = 1000
+        heart_data = []
+        for i in range(501):
+            count += 0.1
+            val = (np.sin(count) + 1) / 2
+            val += (np.sin(count*2+80) + 1) / 2
+            heart_data.append(val)
 
-            # Animation
-            x, y = (15, 23)
-            radius = 4
-            anim = next(self.ha) # Get the next location & scale for the heart sprite
-            y_anim = anim * 15 + y
-            scale = anim * 15 + 15
+        ha = bounce_animation()
 
-            # The heart
-            draw.ellipse((x, y_anim, x+2*radius, y_anim+2*radius), outline="white", fill="white")
-            draw.ellipse((x+2*radius, y_anim, x+4*radius, y_anim+2*radius), outline="white", fill="white")
-            draw.polygon([(x, y_anim+1.48*radius), (x+4*radius, y_anim+1.48*radius), (x+2*radius, y_anim+4*radius)], outline="white", fill="white")
-            # The shadow
-            draw.ellipse((x+radius-scale/2, y+6*radius, x+3*radius+scale/2, y+7*radius), outline="white", fill="white")
+        prev_x = 0
+        prev_y = 0
+
+        while (time.time() - prev_time) < 100:
+            with canvas(device, dither=True) as draw:
+                count += 0.1
+
+                val = (np.sin(count) + 1) / 2
+                val += (np.sin(count*2+80) + 1) / 2
+                val += (np.sin(count/4+10) + 1) / 2
+                heart_data.append(val)
+                heart_data = heart_data[1:]
+
+
+                # Menu options
+                for i, el in enumerate(heart_data):
+                    if True:
+                        x = int(i/10 + 55)
+                        y = (device.height/3 - 1) * el
+                        if i != 0:
+                            draw.polygon((x, y, prev_x, prev_y), fill="white")
+                        prev_x, prev_y = x,y
+
+
+                # Animation
+                x, y = (15, 23)
+                radius = 4
+                anim = next(ha) # Get the next location & scale for the heart sprite
+                y_anim = anim * 15 + y
+                scale = anim * 15 + 15
+
+                # The heart
+                draw.ellipse((x, y_anim, x+2*radius, y_anim+2*radius), outline="white", fill="white")
+                draw.ellipse((x+2*radius, y_anim, x+4*radius, y_anim+2*radius), outline="white", fill="white")
+                draw.polygon([(x, y_anim+1.48*radius), (x+4*radius, y_anim+1.48*radius), (x+2*radius, y_anim+4*radius)], outline="white", fill="white")
+                # The shadow
+                draw.ellipse((x+radius-scale/2, y+6*radius, x+3*radius+scale/2, y+7*radius), outline="white", fill="white")
+
+
+
 
 
 if __name__ == "__main__":
-
-    menu_options = {
-        "options": ["Option 1", "Option 2", "85 bpm", "Option 4"],
-        "cursor": 2,
-    }
-
-    try:
-        device = get_device()
-        m = Menu(menu_options)
-        while True:
-            m.run(device)
-    except KeyboardInterrupt:
-        pass
+    main()
